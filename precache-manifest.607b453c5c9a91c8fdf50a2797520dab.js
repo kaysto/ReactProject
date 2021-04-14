d2cbae2e8272795c1531f092454d25d11edd09b9
Kayci Stone <kaycistone15@gmail.com>
2021-04-13 21:42:52 -0400

    Updates

---
+++ precache-manifest.607b453c5c9a91c8fdf50a2797520dab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af8ba19f6d2b3fe1d8287bad205d432",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "1bc3e84d2f204c922da8",
+    "url": "/ReactProject/static/css/main.d82aae0c.chunk.css"
+  },
+  {
+    "revision": "8c2ab5734eb240732a5f",
+    "url": "/ReactProject/static/js/2.716792c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactProject/static/js/2.716792c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bc3e84d2f204c922da8",
+    "url": "/ReactProject/static/js/main.e5241cfe.chunk.js"
+  },
+  {
+    "revision": "45aaf778174c662558f5",
+    "url": "/ReactProject/static/js/runtime-main.71f4de0d.js"
+  },
+  {
+    "revision": "201b3a3c4f54ae6ec09a021299b7bb8a",
+    "url": "/ReactProject/static/media/groovy.201b3a3c.jpg"
+  }
+]);
